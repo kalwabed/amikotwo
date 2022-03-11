d102446a2212545765192f3f83d0f17bdc4ecd78
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-03-11 08:06:54 +0700

    feat: minimal seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,22 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>AmikoTwo</title>
+        <meta name="description" content="Web base Bot Presensi. Originated from Amikom Bot Presensi." />
+        <meta name="image" content="https://amikotwo.vercel.app/assets/cover.png" />
+        <meta property="og:description" content="Web base Bot Presensi. Originated from Amikom Bot Presensi." />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="AmikoTwo" />
+        <meta property="og:title" content="AmikoTwo" />
+        <meta name="og:image" content="https://amikotwo.vercel.app/assets/cover.png" />
+        <meta name="og:url" content="https://amikotwo.vercel.app" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="icon" href="/favicon.ico" sizes="any" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/assets/apple-touch-icon.png" />
+        <link rel="icon" href="/assets/icon.svg" type="image/svg+xml" />
       </Head>
       <LayoutRoot>
         <Toaster />
